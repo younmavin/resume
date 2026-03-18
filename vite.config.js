3e74060253e4faffe4e46b8323e25a5b444b05d2
younmavin <ddw9819@gmail.com>
2026-03-18 13:49:08 +0900

    vite 경로 변경

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: 'resume', // 👈 여기를 네 GitHub 레포 이름으로 수정
+  base: '/resum/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
